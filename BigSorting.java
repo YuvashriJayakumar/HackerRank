0708a55bc6254cae9ffa323a0275bfe7445a132b
Yuvashri <jyuvashri@hotmail.com>
2018-03-29 14:35:06 +0530

    Update BigSorting.java

--- BigSorting.java
+++ BigSorting.java
@@ -16,7 +16,8 @@ public class Solution {
                 else{
                     for(int i=0;i<a.length();i++){
                         if(a.charAt(i)>b.charAt(i)) return 1;
-                        else return -1;
+                        else if(a.charAt(i)<b.charAt(i)) 
+                            return -1;
                     }
                 }
                 return 0;
